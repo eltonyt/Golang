52d024fa2dfae4b5703356a722fdcd312d1582db
Yutong Li (Elton) <eltonyt.li@gmail.com>
2024-08-19 20:46:23 -0500

    Finished Section 3 - Exercise

---
+++ Section2/Exercise1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Exercise one: printing some emojis 😂🤣❤️😍😁😘👌")
+	var text = "hellow word"
+	fmt.Printf(`String Literal: 1st line %v
+	Next Line: lol`, text)
+}
